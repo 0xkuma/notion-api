a4ac3673f801868fe37444f40af3d0d3d874f4c1
Eddie Cheung <eddie.cheung@ecloudvalley.com>
2022-09-30 14:57:06 +0800

    Change to compare task

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import { Client } from '@notionhq/client';
-import { addItem, queryDB, updateItem, queryItem } from './notion-controller';
+import { addItem, queryDB, queryItem } from './notion-controller';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+interface IClientAwsId {
+  [key: string]: string;
+}
 
 const notion = new Client({
   auth: process.env.NOTION_KEY,
@@ -11,19 +15,25 @@ const taskDatabaseId = process.env.NOTION_TASK_DATABASE_ID!;
 const masterListDatabaseId = process.env.NOTION_MASTER_LIST_DATABASE_ID!;
 const clientProfileDatabaseId = process.env.NOTION_CLIENT_PROFILE_DATABASE_ID!;
 
+// Compare two arrays and find the missing elements
+const getMissingTaskId = (arr1: string[], arr2: string[]) => {
+  const missing = arr1.filter((item) => !arr2.includes(item));
+  return missing;
+};
+
 const getAllTaskId = async () => {
   const res = await queryDB(notion, masterListDatabaseId);
   return res?.results.map((item) => item.id)!;
 };
 
 const getAllClientId = async () => {
-  const res = await queryDB(notion, clientProfileDatabaseId, {
-    property: 'isTaskCreated',
-    checkbox: {
-      equals: false,
-    },
-  });
-  return res?.results.map((item) => item.id)!;
+  const res = await queryDB(notion, clientProfileDatabaseId);
+  let clientAwsIdList: IClientAwsId = {};
+  for (let i = 0; i < res?.results.length!; i++) {
+    const clientId = await queryItem(notion, res?.results[i].id!);
+    clientAwsIdList[clientId?.properties['AWS ID'].rich_text[0].plain_text!] = res?.results[i].id!;
+  }
+  return clientAwsIdList;
 };
 
 const getAllTaskIdCreatedByClientId = async (clientId: string) => {
@@ -39,7 +49,7 @@ const getAllTaskIdCreatedByClientId = async (clientId: string) => {
     },
   });
   let existTaskId: string[] = [];
-  for(let i = 0; i < getAllTaskIdRes?.results.length!; i++) {
+  for (let i = 0; i < getAllTaskIdRes?.results.length!; i++) {
     const res = await queryItem(notion, getAllTaskIdRes?.results[i].id!);
     existTaskId.push(res?.properties['Task Full Name'].relation[0].id!);
   }
@@ -47,18 +57,17 @@ const getAllTaskIdCreatedByClientId = async (clientId: string) => {
 };
 
 export const handler = async (event: any) => {
-  const clientIdList = await getAllClientId();
   const taskIdList = await getAllTaskId();
+  const clientIdList = await getAllClientId();
 
-  clientIdList.map((clientId) => {
-    taskIdList.map((taskId) => {
-      addItem(notion, taskDatabaseId, clientId, taskId);
-    });
-    updateItem(notion, clientId, {
-      isTaskCreated: {
-        checkbox: true,
-      },
-    });
+  Object.entries(clientIdList).forEach(async ([key, value]) => {
+    const existTaskId = await getAllTaskIdCreatedByClientId(key);
+    const missingTaskId = getMissingTaskId(taskIdList, existTaskId);
+    if (missingTaskId.length > 0) {
+      for (let i = 0; i < missingTaskId.length; i++) {
+        await addItem(notion, taskDatabaseId, value, missingTaskId[i]);
+      }
+    }
   });
 };
 handler(null);
